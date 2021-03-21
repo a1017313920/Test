f008bb44bf47b5e48df6cb407f1a9dd189bf1f2c
1017313920@qq.com <1017313920@qq.com>
2021-03-21 22:16:12 +0800

    第二次

---
+++ Assets/Scenes/Script/Test2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour
+{
+    public string test = "11111";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
